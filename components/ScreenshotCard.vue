d8d1a21b7eac66b3c64fb07e9d7151552df1f5c5
Lyle Wilner <floopikiii@gmail.com>
2024-07-30 19:27:27 -0700

    fix width

--- components/ScreenshotCard.vue
+++ components/ScreenshotCard.vue
@@ -3,6 +3,7 @@
     class="mx-auto my-8 title-card"
     :elevation="elevation"
     max-height="700"
+    width="400"
     height="700"
     @mouseover="addElevation()"
     @mouseleave="elevation = 0"
